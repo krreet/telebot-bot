e8089bb106bed51b9161b1b0d2d240388e6fd7f8
Reet <reet1plus1@gmail.com>
2018-03-21 00:13:19 +0530

    ok

--- bot.js
+++ bot.js
@@ -32,12 +32,17 @@ let replymessage = 'This code is not valid';
 
             if(doc.points === 0){
 
-                User.update( { _id : text } , { $set : { points : 1 } } ).exec().then( res => { console.log(res);replymessage = `your code: ${text} is activated successfully, send shared link to your friend right away to get your bonus. Make sure you have subscribed the channel (@officialsapien) to receive your tokens.
+                User.update( { _id : text } , { $set : { points : 1 } } ).exec().then( res => { console.log(res);
+                    
+                    
+                    replymessage = `your code: ${text} is activated successfully, send shared link to your friend right away to get your bonus. Make sure you have subscribed the channel (@officialsapien) to receive your tokens.
 
                 你的验证码：${text}，已激活成功！立刻发送分享链接给朋友获得空投奖励！请确保已经关注频道（@officialsapien）以正常获得空投奖励。
                 
                 Your share link （你的分享链接）：https://sapien-network.herokuapp.com/?r=${text}`; 
-                return  bot.sendMessage(msg.from.id, replymessage, { replyToMessage: msg.message_id });
+
+
+                return  bot.sendMessage('@sapienglobal', replymessage, { replyToMessage: msg.message_id });
             
             
             }).catch(err => {console.log(err);
